9a26037c459286a97cd0b735a81fa41ef8cf2652
Baron Chong <baronchong23@gmail.com>
2025-02-15 23:35:53 +0800

    Iteration 2:Loose Coupling - Interfaces

--- learn-spring-framework/src/main/java/com/spring/learn_spring_framework/LearnSpringFrameworkApplication.java
+++ learn-spring-framework/src/main/java/com/spring/learn_spring_framework/LearnSpringFrameworkApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.spring.learn_spring_framework.game.GameRunner;
 import com.spring.learn_spring_framework.game.MarioGame;
+import com.spring.learn_spring_framework.game.PacmanGame;
 import com.spring.learn_spring_framework.game.SuperContraGame;
 
 @SpringBootApplication
@@ -13,11 +14,16 @@ public class LearnSpringFrameworkApplication {
 //		SpringApplication.run(LearnSpringFrameworkApplication.class, args);
 		MarioGame game = new MarioGame();
 		SuperContraGame game2 = new SuperContraGame();
+		PacmanGame game3 = new PacmanGame();
 
 		GameRunner runner = new GameRunner(game);
-		runner.run();
+//		runner.run();
+		runner.run3();
 		GameRunner runner2 = new GameRunner(game2);
-		runner2.run2();
+//		runner2.run2();
+		runner2.run3();
+		GameRunner runner3 = new GameRunner(game3);
+		runner3.run3();
 
 
 	}
--- learn-spring-framework/src/main/java/com/spring/learn_spring_framework/game/GameRunner.java
+++ learn-spring-framework/src/main/java/com/spring/learn_spring_framework/game/GameRunner.java
@@ -2,30 +2,42 @@ package com.spring.learn_spring_framework.game;
 
 public class GameRunner {
 	
-	private MarioGame game;
-	private SuperContraGame game2;
+	// private MarioGame game;
+	// private SuperContraGame game2;
+	private GamingConsole game3;
 
 	
-	public GameRunner(MarioGame game) {
-		this.game = game;
-	}
+	// public GameRunner(MarioGame game) {
+	// 	this.game = game;
+	// }
 	
-	public GameRunner(SuperContraGame game) {
-		this.game2 = game;
+	// public GameRunner(SuperContraGame game) {
+	// 	this.game2 = game;
+	// }
+	
+	public GameRunner(GamingConsole game) {
+		this.game3 = game;
 	}
 
-	public void run() {
-		game.up();
-		game.down();
-		game.left();
-		game.right();
-	}
+	// public void run() {
+	// 	game.up();
+	// 	game.down();
+	// 	game.left();
+	// 	game.right();
+	// }
 	
-	public void run2() {
-		game2.up();
-		game2.down();
-		game2.left();
-		game2.right();
+	// public void run2() {
+	// 	game2.up();
+	// 	game2.down();
+	// 	game2.left();
+	// 	game2.right();
+	// }
+	
+	public void run3() {
+		game3.up();
+		game3.down();
+		game3.left();
+		game3.right();
 	}
 
 }
