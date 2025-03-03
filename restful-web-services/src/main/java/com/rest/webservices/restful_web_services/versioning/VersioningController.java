045caadc361770494a873536ec176b7d7d4192c6
Baron Chong <baronchong23@gmail.com>
2025-03-03 22:50:58 +0800

    Versioning REST API - Request Param, Header and Content Negotiation

--- restful-web-services/src/main/java/com/rest/webservices/restful_web_services/versioning/VersioningController.java
+++ restful-web-services/src/main/java/com/rest/webservices/restful_web_services/versioning/VersioningController.java
@@ -1,6 +1,7 @@
 package com.rest.webservices.restful_web_services.versioning;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,4 +16,35 @@ public class VersioningController {
 	public PersonV2 getV2ofPerson() {
 		return new PersonV2(new Name("Bob", "Charlie"));
 	}
+	
+	@GetMapping(path = "/person", params = "version=1")
+	public PersonV1 getV1ofPersonByParams() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(path = "/person", params = "version=2")
+	public PersonV2 getV2ofPersonByParams() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	
+	@GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+	public PersonV1 getV1ofPersonByReqHeader() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+	public PersonV2 getV2ofPersonByReqHeader() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	
+	@GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+	public PersonV1 getV1ofPersonByContentNegotiation() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+	public PersonV2 getV2ofPersonByContentNegotiation() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	
 }
