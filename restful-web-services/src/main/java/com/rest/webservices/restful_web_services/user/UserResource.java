1d17531bc6c5471f6d8cab1b8feb58cbc7a08f28
Baron Chong <baronchong23@gmail.com>
2025-03-04 00:01:52 +0800

    Implementing HATEOAS for REST API

--- restful-web-services/src/main/java/com/rest/webservices/restful_web_services/user/UserResource.java
+++ restful-web-services/src/main/java/com/rest/webservices/restful_web_services/user/UserResource.java
@@ -1,8 +1,11 @@
 package com.rest.webservices.restful_web_services.user;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import java.net.URI;
 import java.util.List;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,15 +31,23 @@ public class UserResource {
 		return service.findAllUser();
 	}
 	
+	//http://localhost:8080/users
+	//EntityModel
+	//WebMvcLinkBuilder
 	@GetMapping(path = "/users/{id}")
-	public User retrieveUsersById(@PathVariable int id) {
+	public EntityModel<User> retrieveUsersById(@PathVariable int id) {
 		User user = service.findOne(id);
 		
 		if (user == null) {
 			throw new UserNotFoundException("id:" + id);
 		}
 		
-		return user;
+		EntityModel<User> entityModel = EntityModel.of(user);
+		
+		WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		entityModel.add(link.withRel("all-users"));
+		
+		return entityModel;
 	}
 	
 	@DeleteMapping(path = "/users/{id}")
--- restful-web-services/src/main/java/com/rest/webservices/restful_web_services/versioning/VersioningController.java
+++ restful-web-services/src/main/java/com/rest/webservices/restful_web_services/versioning/VersioningController.java
@@ -1,7 +1,6 @@
 package com.rest.webservices.restful_web_services.versioning;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
